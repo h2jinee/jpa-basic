b6d8cdc769470f843e30656970097c2b63234f71
h2jinee <wjsgmlwls97@gmail.com>
2023-12-12 18:07:37 +0900

    Hello JPA - 애플리케이션 개발

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,2 +1,16 @@
-package hellojpa;public class JpaMain {
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        // code
+        em.close();
+        emf.close();
+    }
 }
