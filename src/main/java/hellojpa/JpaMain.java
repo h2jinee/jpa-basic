8ef49db381c075473e198874c63162e001f6f48c
h2jinee <wjsgmlwls97@gmail.com>
2023-12-13 18:41:16 +0900

    영속성 관리 - 내부 동작 방식

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,15 +17,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            // 영속
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
+
+            em.clear();
+
+            Member member2 = em.find(Member.class, 150L);
+            System.out.println("=====================================");
 
             tx.commit();
         } catch (Exception e) {
