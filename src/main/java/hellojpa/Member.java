91a7ca6876b5681e628f8f9babb4f3e55eec75cf
HEEJIN JEON <wjsgmlwls97@gmail.com>
2023-12-26 22:39:09 +0900

    데이터베이스 스키마 자동 생성

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -10,6 +11,8 @@ public class Member {
 
     @Id
     private Long id;
+
+    @Column(unique = true, length = 10)
     private String name;
 
     public Long getId() {
